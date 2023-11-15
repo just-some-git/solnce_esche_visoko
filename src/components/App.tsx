cb75b65ced9e9e2653a4b5cdfc6a7d60155918c1
Dmitriy <aegis.ko20@mail.ru>
2023-11-15 15:52:56 +0300

    new design

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,11 +5,17 @@ import useRecordQuestion from '../hooks/useRecordQuestion';
 const App: FC = () => {
 	const [isMicro, setIsMicro] = useState<boolean>(true);
 
-	const [animNeznaika, setAnimNeznaika] = useState<string>('i_do_not_no_hello');
+	const [animNeznaika, setAnimNeznaika] = useState<string>(
+		'i_do_not_know_hello'
+	);
 	const [durationAudio, setDurationAudio] = useState<number>(0);
 	const [textRequest, setTextRequest] = useState<string>('');
 
-	let text = '';
+	const [isWaitAnswer, setIsWaitAnswer] = useState<boolean>(false);
+
+	// let text = '';
+	const [text, setText] = useState();
+	const [test, setTest] = useState(false);
 
 	let nameAudio;
 
@@ -18,19 +24,29 @@ const App: FC = () => {
 	const [viewResponce, setViewResponce] = useState<boolean>(false);
 
 	const { startReq, recognition, stopReq } = useRecordQuestion();
+	// const {
+	// 	receiveAudioStream,
+	// 	receivedAudioUrl,
+	// 	addText,
+	// 	text,
+	// 	setText,
+	// 	test,
+	// 	setTest,
+	// 	textRequest,
+	// 	durationAudio,
+	// } = useRequestQuestion();
 
 	useEffect(() => {
 		console.log('use:', text);
 	}, [text]);
 
 	useEffect(() => {
-		let anim = setTimeout(() => {
-			// setAnimNeznaika('i_do_not_no_wait');
+		// когда время длительности аудио проходит, убирает текст ответа. А другой юз эффект меняет анимацию
+		let stopAnim = setTimeout(() => {
 			setViewResponce(false);
 		}, durationAudio);
-
 		return () => {
-			clearTimeout(anim);
+			clearTimeout(stopAnim);
 		};
 	}, [durationAudio]);
 
@@ -40,7 +56,7 @@ const App: FC = () => {
 		const audio = new Audio(audioUrl);
 		audio.play();
 		setViewResponce(true);
-		setAnimNeznaika('i_do_not_no');
+		setAnimNeznaika('i_do_not_know');
 	};
 
 	const getAudioDuration = audioUrl => {
@@ -74,7 +90,7 @@ const App: FC = () => {
 			);
 			const audioStream = response.body;
 
-			// console.log('header', [...response.headers.entries()]);
+			// console.log('header', [...response.headers.entries()]); //перебор заголовков
 
 			const reader = audioStream.getReader();
 			const audioChunks = [];
@@ -98,7 +114,7 @@ const App: FC = () => {
 			/////
 			getAudioDuration(audioUrl)
 				.then(duration => {
-					console.log('Длительность аудиофайла:', duration, 'секунд');
+					console.log('Длительность аудиофайла:', duration, 'миллисекунд');
 					setDurationAudio(duration);
 				})
 				.catch(error => {
@@ -123,17 +139,6 @@ const App: FC = () => {
 		}
 	};
 
-	// const handleMicroClickStop = async () => {
-	// 	if (!isMicro) {
-	// 		await stopReq();
-	// 		await receiveAudioStream();
-	// 		await addText();
-	// 		await playAudio(receivedAudioUrl);
-	// 		console.log('stop');
-	// 		setIsMicro(!isMicro);
-	// 	}
-	// };
-
 	const handleMicroClickPlay = () => {
 		if (isMicro) {
 			startReq();
@@ -143,63 +148,193 @@ const App: FC = () => {
 	};
 
 	useEffect(() => {
+		//HELP: ОСНОВНОЙ ВАРИАНТ АНИМАЦИИ
+		// когда исчезает текст ответа, меняет анимацию на начальную
 		if (!viewResponce) {
-			setAnimNeznaika('i_do_not_no_hello');
+			setAnimNeznaika('i_do_not_know_hello');
+		} else {
+			// if (textRequest === 'булка') {
+			// 	let anim = setTimeout(() => {
+			// 		setAnimNeznaika('i_do_not_know_wait');
+			// 		console.log('poluchilos');
+			// 	}, durationAudio - 3000);
+
+			// 	return () => {
+			// 		clearTimeout(anim);
+			// 	};
+			// } else if (textRequest === 'пусто') {
+			// 	let anim = setTimeout(() => {
+			// 		setAnimNeznaika('negative_response');
+			// 		console.log('poluchilos');
+			// 	}, durationAudio - 3000);
+
+			// 	return () => {
+			// 		clearTimeout(anim);
+			// 	};
+
+			if (durationAudio <= 6000) {
+				let anim = setTimeout(() => {
+					setAnimNeznaika('i_do_not_know_wait');
+				}, durationAudio - 3000);
+
+				return () => {
+					clearTimeout(anim);
+				};
+			} else {
+				let wait = setTimeout(() => {
+					setIsWaitAnswer(true);
+				}, 3000);
+
+				let anim = setTimeout(() => {
+					setIsWaitAnswer(false);
+					setAnimNeznaika('i_do_not_know_wait');
+				}, durationAudio - 3000);
+
+				return () => {
+					clearTimeout(anim);
+					clearTimeout(wait);
+				};
+			}
 		}
 	}, [viewResponce]);
 
+	// useEffect(() => { //HELP: ЗАПАСНОЙ ВАРИАНТ АНИМАЦИИ
+	// 	if (!viewResponce) {
+	// 		setAnimNeznaika('i_do_not_know_hello');
+	// 	} else {
+	// 		let anim = setTimeout(() => {
+	// 			setAnimNeznaika('i_do_not_know_wait');
+	// 		}, durationAudio - 3000);
+
+	// 		return () => {
+	// 			clearTimeout(anim);
+	// 		};
+	// 	}
+	// }, [viewResponce]);
+
 	recognition.onresult = async function (event) {
 		const transcript = event.results[0][0].transcript;
-		text = transcript;
-
+		//Если делать черезе переменную test a не useState// text = transcript;
+		setText(transcript);
 		if (event.results[0].isFinal) {
 			setIsMicro(true);
 		}
-
-		await receiveAudioStream();
-		await addText();
-		await playAudio(receivedAudioUrl);
+		//Если делать черезе переменную test a не useState // await receiveAudioStream();
+		// await addText();
+		// await playAudio(receivedAudioUrl);
+		setTest(true);
 	};
 
-	return (
-		<div className='wrapper__app'>
-			<button
-				onClick={() => {
-					setAnimNeznaika('i_do_not_no');
-					setTimeout(() => setAnimNeznaika('i_do_not_no_hello'), 3000);
-				}}
-			>
-				кнопка 1
-			</button>
-			<button onClick={() => setAnimNeznaika('i_do_not_no_wait')}>
-				кнопка 2
-			</button>
-			<button onClick={() => setAnimNeznaika('i_do_not_no_hello')}>
-				кнопка 3
-			</button>
-			{!viewResponce ? (
-				<img className='app__hello' src='./images/hello.png' alt='hello' />
-			) : (
-				<p className='text_answer'>{textRequest}</p>
-			)}
-			<div
-				className='animation'
-				style={{ animation: `${animNeznaika} 3s linear infinite` }}
-			></div>
+	useEffect(() => {
+		const fetchData = async () => {
+			await receiveAudioStream();
+			await addText();
+			await playAudio(receivedAudioUrl);
+		};
 
-			<div className='block__app__micro'>
-				{isMicro ? (
-					<img
-						className='app__micro'
-						src='./images/micro.png'
-						alt='micro'
-						onClick={handleMicroClickPlay}
-					/>
-				) : (
-					<div className='app__micro_active'></div>
-				)}
-			</div>
-		</div>
+		if (test) {
+			fetchData();
+			setTest(false);
+		}
+	}, [test]);
+	//////////////////
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		//таблетка от бликов
+		const animLoading = setTimeout(() => {
+			setIsLoading(false);
+		}, 13000);
+
+		return () => {
+			clearTimeout(animLoading);
+		};
+	}, []);
+	const arrAnimation = [
+		'i_do_not_know',
+		'i_do_not_know_wait',
+		'i_do_not_know_hello',
+		'negative_response',
+		'micro',
+		'bg_main',
+	];
+	//////////////////////////
+
+	return (
+		<>
+			{isLoading ? (
+				<div className='wrapper__loading'>
+					{arrAnimation.map(anim => {
+						return (
+							<div
+								key={anim}
+								className='test'
+								style={{ animation: `${anim} 3s linear infinite` }}
+							></div>
+						);
+					})}
+
+					<svg
+						className='svg-animation'
+						width='100'
+						height='100'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<circle
+							cx='50'
+							cy='50'
+							r='40'
+							stroke='#3498db'
+							strokeWidth='4'
+							fill='none'
+						>
+							<animate
+								attributeName='stroke-dashoffset'
+								dur='2s'
+								from='0'
+								to='502'
+								repeatCount='indefinite'
+							/>
+							<animate
+								attributeName='stroke-dasharray'
+								dur='2s'
+								values='150.79644737231007 100.53096491487338;1 250;150.79644737231007 100.53096491487338'
+								repeatCount='indefinite'
+							/>
+						</circle>
+					</svg>
+				</div>
+			) : (
+				<div className='wrapper__app'>
+					{!viewResponce ? (
+						<img className='app__hello' src='./images/hello.png' alt='hello' />
+					) : (
+						<p className='text_answer'>{textRequest}</p>
+					)}
+					{isWaitAnswer ? (
+						<div className='animation_wait'></div>
+					) : (
+						<div
+							className='animation'
+							style={{ animation: `${animNeznaika} 3s linear infinite` }}
+						></div>
+					)}
+
+					<div className='block__app__micro'>
+						{isMicro ? (
+							<img
+								className='app__micro'
+								src='./images/micro.png'
+								alt='micro'
+								onClick={handleMicroClickPlay}
+							/>
+						) : (
+							<div className='app__micro_active'></div>
+						)}
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
